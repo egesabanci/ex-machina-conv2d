91aa147c2cefcbdc2ca306439c8ad65580014952
Ege Sabanci <egesabanci@outlook.com.tr>
2021-12-16 05:40:36 +0300

    output has been cropped after saved- bash script updated - 16/12/2021

--- scripts/create.sh
+++ scripts/create.sh
@@ -3,5 +3,6 @@
 rm -r ../target/dist
 mkdir ../target/dist
 ffmpeg -framerate $1 -i ../target/final-frames/%03d.jpg -c:v libx264 ../target/dist/output.mp4
-ffmpeg -i ../target/dist/output.mp4 ../target/dist/output.gif
-rm ../target/dist/output.mp4
+ffmpeg -i ../target/dist/output.mp4 -filter:v "crop=1280:540:0:100" ../target/dist/cropped.mp4
+ffmpeg -i ../target/dist/cropped.mp4 ../target/dist/output.gif
+rm ../target/dist/*.mp4
